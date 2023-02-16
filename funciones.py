ae7e83f8b41f578a1004f51b47092408d77dd22a
oier <oieruni22@gmail.com>
2023-02-16 19:53:09 +0100

    [ADD] Adding updateSuccessors (updates the path and the cost of every successor of a parent state) and pushPQueue (pushes a successor into a priority queue) function

--- funciones.py
+++ funciones.py
@@ -14,3 +14,19 @@ def setSuccessorsPaths(directions, successors, parentPath):
         if s[0] not in keys:
             directions[s[0]] = parentPath + [s[1]]
     return directions
+
+
+def updateSuccessors(successors, parent, visited):
+    successorsUpdated = []
+    for s in successors:
+        if s[0] not in visited:
+            successorsUpdated.append(
+                (s[0], parent[1] + [s[1]], parent[2] + s[2]))
+
+    return successorsUpdated
+
+
+def pushPQueue(successors, pqueue):
+    for s in successors:
+        pqueue.push(s, s[2])
+    return pqueue
