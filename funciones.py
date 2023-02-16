161bbfee8919a07155f842c20f35eb041f8930b7
oier <oieruni22@gmail.com>
2023-02-16 12:40:56 +0100

    [ADD] Adding setSuccessorsPath function to add the path of a child node in an object

--- funciones.py
+++ funciones.py
@@ -1,9 +1,16 @@
-def pushSuccessors(successors, stack, seen):
+def pushSuccessors(successors, structure, seen):
     for s in successors:
-        if s[0] not in seen:
-            stack.push(s)
-    return stack
+        structure.push(s)
+    return structure
 
 
 def isStateVisited(visited, state):
     return state in visited
+
+
+def setSuccessorsPaths(directions, successors, parentPath):
+    keys = directions.keys()
+    for s in successors:
+        if s[0] not in keys:
+            directions[s[0]] = parentPath + [s[1]]
+    return directions
